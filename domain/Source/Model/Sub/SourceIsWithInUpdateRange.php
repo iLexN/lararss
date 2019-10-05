6a0a17cf3ca53ef83fd234f22d7f789a069f19eb
iLexN <ilex.job@gmail.com>
2019-10-05 16:50:05 +0800

    fix last sync is null

--- domain/Source/Model/Sub/SourceIsWithInUpdateRange.php
+++ domain/Source/Model/Sub/SourceIsWithInUpdateRange.php
@@ -14,6 +14,10 @@ final class SourceIsWithInUpdateRange
 
     public function __invoke(Source $source)
     {
+        if ($source->last_sync === null){
+            return true;
+        }
+
         $now = Carbon::now();
         return $source->last_sync->diffInHours($now, false) > self::UPDATED_RANGE_HOUR;
     }
