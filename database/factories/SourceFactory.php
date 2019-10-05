4d9750df1a752816d1e4a17fc450e014580e1fcf
iLexN <ilex.job@gmail.com>
2019-10-05 16:06:46 +0800

    update seed for last sync

--- database/factories/SourceFactory.php
+++ database/factories/SourceFactory.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 use Domain\Source\DbModel\Source;
 use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Support\Carbon;
 
 /** @var Factory $factory */
 $factory->define(Source::class, static function (Faker $faker) {
     return [
         'url' => $faker->url,
         'status' => 1,
+        'last_sync' => Carbon::now(),
     ];
 });
